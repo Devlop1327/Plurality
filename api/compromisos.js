ab629067c07c62427471f8f59824e774697e9d2e
Devlop1327 <primevideo1327@gmail.com>
2026-06-16 12:39:40 -0500

    Fix: disable caching for api routes

--- api/compromisos.js
+++ api/compromisos.js
@@ -44,10 +44,11 @@ async function getRedisClient() {
 }
 
 export default async function handler(req, res) {
-    // Enable CORS
+    // Enable CORS & Disable Caching
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,DELETE,POST,OPTIONS');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
